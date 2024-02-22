08200c06b7757e7eba7f226e64ebdda84e0e801f
Jeremias1994 <141497428+Jeremias1994@users.noreply.github.com>
2024-02-22 17:39:17 -0300

    Código feito acompanhando a video aula.

--- ALURA/src/main/java/ProjetoAlura/DesafioAlura.java
+++ ALURA/src/main/java/ProjetoAlura/DesafioAlura.java
@@ -11,29 +11,34 @@ public class DesafioAlura {
         int opcoes = 0;
         int valor = 0;
         int resultado = 0;
+        int menu = 0;
         nome = "Jeremias Inacio";
         tipoDeConta = "Conta corrente.";
         saldoInicial = 2500;
 
-
-        System.out.println("Dados Iniciais do Cliente");
-        System.out.println("****************************************************************");
-        System.out.println(" Nome: " + nome);
-        System.out.println(" Tipo de Conta: " + tipoDeConta);
-        System.out.println(" Saldo inicial: " + saldoInicial);
-        System.out.println("****************************************************************");
-
-        System.out.println("Operações");
-
-        System.out.println("1- Consultar dados: ");
-        System.out.println("2- Receber valor: ");
-        System.out.println("3- Transferir valor: ");
-        System.out.println("4- Sair: ");
-        System.out.println("Insira um numero para opção desejada: ");
-        opcoes = dados.nextInt();
+        System.out.println("Deseja Acessar o Sistema: ");
+        System.out.println("Digite 1- Sim e 2- Não");
+        menu = dados.nextInt();
 
 
-        while (opcoes <= 4) {
+
+        while (menu == 1) {
+
+            System.out.println("Dados Iniciais do Cliente");
+            System.out.println("****************************************************************");
+            System.out.println(" Nome: " + nome);
+            System.out.println(" Tipo de Conta: " + tipoDeConta);
+            System.out.println(" Saldo da Conta: " + saldoInicial);
+            System.out.println("****************************************************************");
+
+            System.out.println("Operações");
+
+            System.out.println("1- Consultar dados: ");
+            System.out.println("2- Receber valor: ");
+            System.out.println("3- Transferir valor: ");
+            System.out.println("4- Sair: ");
+            System.out.println("Insira um numero para opção desejada: ");
+            opcoes = dados.nextInt();
 
 
             if (opcoes == 1) {
@@ -41,75 +46,40 @@ public class DesafioAlura {
                 System.out.println("****************************************************************");
                 System.out.println(" Nome: " + nome);
                 System.out.println(" Tipo de Conta: " + tipoDeConta);
-                System.out.println(" Saldo inicial: " + saldoInicial);
+                System.out.println(" Saldo da Conta: " + saldoInicial);
                 System.out.println("****************************************************************");
-
-                System.out.println("Deseja fazer algo mais ? informe abaixo qual opção: ");
-                System.out.println("1- Consultar dados: ");
-                System.out.println("2- Receber valor: ");
-                System.out.println("3- Transferir valor: ");
-                System.out.println("4- Sair: ");
-                System.out.println("Insira um numero para opção desejada: ");
-                opcoes = dados.nextInt();
-
-
             }
             if (opcoes == 2) {
                 System.out.println("Informe o Valor: ");
                 valor = dados.nextInt();
                 resultado = saldoInicial + valor;
-                System.out.println("Dados Iniciais do Cliente");
-                System.out.println("****************************************************************");
-                System.out.println(" Nome: " + nome);
-                System.out.println(" Tipo de Conta: " + tipoDeConta);
-                System.out.println(" Saldo inicial: " + saldoInicial + " Saldo atual: " + resultado);
-                System.out.println("****************************************************************");
-
-                System.out.println("Deseja fazer algo mais ? informe abaixo qual opção: ");
-                System.out.println("1- Consultar dados: ");
-                System.out.println("2- Receber valor: ");
-                System.out.println("3- Transferir valor: ");
-                System.out.println("4- Sair: ");
-                System.out.println("Insira um numero para opção desejada: ");
-                opcoes = dados.nextInt();
-
-
+                saldoInicial = resultado;
             }
-            if (opcoes == 3 ) {
+            if (opcoes == 3) {
                 System.out.println("Qual valor deseja transferir: ");
                 valor = dados.nextInt();
-                resultado = saldoInicial - valor;
-                System.out.println("Dados Atualizados do Cliente");
-                System.out.println("****************************************************************");
-                System.out.println(" Nome: " + nome);
-                System.out.println(" Tipo de Conta: " + tipoDeConta);
-                System.out.println(" Saldo inicial: " + saldoInicial + " Saldo atual: " + resultado);
-                System.out.println("****************************************************************");
-
-                System.out.println("Deseja fazer algo mais ? informe abaixo qual opção: ");
-                System.out.println("1- Consultar dados: ");
-                System.out.println("2- Receber valor: ");
-                System.out.println("3- Transferir valor: ");
-                System.out.println("4- Sair: ");
-                System.out.println("Insira um numero para opção desejada: ");
-                opcoes = dados.nextInt();
-
-
-            }if (valor > 2500) {
+                if (valor > 2500) {
                     System.out.println("Saldo insuficiente! Seu pobre fudido!");
-                    break;
+                    continue;
+
                 }
+                resultado = saldoInicial - valor;
+                saldoInicial = resultado;
+            }
             if (opcoes == 4){
                 System.out.println("Nada mais Show! ");
                 break;
             }
+        }
+        if (menu ==2){
+            System.out.println("Fim.");
+        }
+        if (menu >= 3 || menu == 0 ){
+            System.out.println("Opção inválida.");
 
-
-
-
-            }
         }
     }
+}
 
 
 
---
+++ ALURA/src/main/java/ProjetoAlura/DesafioAlura2.java
@@ -0,0 +1,88 @@
+package ProjetoAlura;
+
+import java.util.Scanner;
+
+public class DesafioAlura2 {
+    public static void main(String[] args) {
+        int contadorInicial = 0;
+        int contadorFinal = 1;
+        Scanner dados = new Scanner(System.in);
+        String nome;
+        String tipoDeConta;
+        int saldoInicial;
+        int opcoes = 0;
+        int valor = 0;
+        int resultado = 0;
+        int menu = 0;
+        nome = "Jeremias Inacio";
+        tipoDeConta = "Conta corrente.";
+        saldoInicial = 2500;
+
+        do {
+            System.out.println("Deseja Acessar o Sistema: ");
+            System.out.println("Digite 1- Sim e 2- Não");
+            menu = dados.nextInt();
+
+
+           if (menu >=3 || menu == 0){
+               System.out.println("Opção inválida, selecione 1 ou 2");
+           }
+           if (menu == 2){
+               System.out.println("Obrigado e volte sempre!");
+               contadorFinal = 0;
+           }
+
+            while (menu == 1) {
+
+                System.out.println("Dados Iniciais do Cliente");
+                System.out.println("****************************************************************");
+                System.out.println(" Nome: " + nome);
+                System.out.println(" Tipo de Conta: " + tipoDeConta);
+                System.out.println(" Saldo da Conta: " + saldoInicial);
+                System.out.println("****************************************************************");
+
+                System.out.println("Operações");
+
+                System.out.println("1- Consultar dados: ");
+                System.out.println("2- Receber valor: ");
+                System.out.println("3- Transferir valor: ");
+                System.out.println("4- Sair: ");
+                System.out.println("Insira um numero para opção desejada: ");
+                opcoes = dados.nextInt();
+
+
+                if (opcoes == 1) {
+                    System.out.println("Dados Iniciais do Cliente");
+                    System.out.println("****************************************************************");
+                    System.out.println(" Nome: " + nome);
+                    System.out.println(" Tipo de Conta: " + tipoDeConta);
+                    System.out.println(" Saldo da Conta: " + saldoInicial);
+                    System.out.println("****************************************************************");
+                }
+                if (opcoes == 2) {
+                    System.out.println("Informe o Valor: ");
+                    valor = dados.nextInt();
+                    resultado = saldoInicial + valor;
+                    saldoInicial = resultado;
+                }
+                if (opcoes == 3) {
+                    System.out.println("Qual valor deseja transferir: ");
+                    valor = dados.nextInt();
+                    if (valor > saldoInicial) {
+                        System.out.println("Saldo insuficiente! Seu pobre fudido!");
+                        continue;
+
+                    }
+                    resultado = saldoInicial - valor;
+                    saldoInicial = resultado;
+                }
+                if (opcoes == 4){
+                    System.out.println("Nada mais Show! ");
+                    break;
+                }
+            }
+
+
+        } while (contadorInicial < contadorFinal);
+    }
+}
